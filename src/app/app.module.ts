b400bc4cf729bb349e8424a13eeb51b235e3943b
EstherMalam <ester_l@malam.com>
2022-11-08 16:28:29 +0200

    add routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './components/menu/menu.component';
@@ -8,20 +7,29 @@ import { HomeComponent } from './components/home/home.component';
 import { SendRequestComponent } from './components/send-request/send-request.component';
 import { ReactiveFormsModule } from "@angular/forms";
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule,Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
 
+const ROUTES:Routes=[
+{path:"request",component:SendRequestComponent},
+{path:"home",component:HomeComponent},
+{path:"about",component:AboutComponent},
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     HomeComponent,
-    SendRequestComponent
+    SendRequestComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent,HomeComponent]
